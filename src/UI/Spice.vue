7031b4368cc18086d0782c2449817d14afe77c94
Abeer <abeer.technology@gmail.com>
2021-03-25 19:29:24 +0600

    added all spices

---
+++ src/UI/Spice.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="spice">
+    <div>
+      <img
+        :src="require('../prodImages/spices/' + spice.imgName + '.jpg')"
+        :alt="spice.imgName"
+      />
+      <h4 class="name">{{ spice.name }}</h4>
+      <p>{{ spice.quantity }}</p>
+      <p>
+        <span class="price text-danger">{{ spice.price }}</span>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["spice"],
+};
+</script>
+
+<style scoped>
+.spice {
+  display: inline-block;
+  text-align: center;
+  margin-right: 20px;
+}
+.spice img {
+  width: 200px;
+}
+.spice h4 {
+  font-weight: 300;
+}
+.spice .price {
+  font-size: 18px;
+  font-weight: 600;
+}
+</style>
--- src/pages/Spices.vue
+++ src/pages/Spices.vue
@@ -1,10 +1,56 @@
 <template>
-  <div>asdf</div>
+  <div class="spices">
+    <ads-on-top></ads-on-top>
+    <div class="headline">
+      <hr />
+      <h4 class="name">Spices</h4>
+      <hr />
+    </div>
+    <div class="all-types">
+      <spice
+        v-for="spice in spices"
+        :key="spice.imgName"
+        :spice="spice"
+      ></spice>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapGetters } from "vuex";
+
+import AdsOnTop from "../components/AdsOnTop.vue";
+import Spice from "../UI/Spice.vue";
+
+export default {
+  components: {
+    AdsOnTop,
+    Spice,
+  },
+  computed: {
+    ...mapGetters(["spices"]),
+  },
+};
 </script>
 
-<style>
+<style scoped>
+.spices {
+  margin: 0 20px;
+}
+.headline {
+  display: flex;
+  flex-direction: row;
+}
+hr {
+  width: 100%;
+  height: 1px;
+  border: none;
+  background-color: rgb(161, 161, 161);
+}
+.name {
+  text-align: center;
+  width: 500px;
+  margin: 0 20px;
+  font-weight: 300;
+}
 </style>
